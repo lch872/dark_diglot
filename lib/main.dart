356d9ae97e561c261ed4afbc08aab9db05a730c2
Larpoux <larpoux@canardoux.xyz>
2019-04-30 15:46:36 +0200

    test Git

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget
         {
                 return MaterialApp
                         (
-                        title: 'Dark Diglot',
+                        title: 'zzzDark Diglot',
                         theme: ThemeData
                                 (
                                 // This is the theme of your application.
